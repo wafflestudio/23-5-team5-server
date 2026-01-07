0ce02fc14177dba27cc64a32d0d9abe602bea371
donghyun <joshua2031@snu.ac.kr>
2026-01-07 23:01:44 +0900

    ci/cd, docker 등 기본 환경 세팅

---
+++ src/test/kotlin/com/team_5/studygroup/StudygroupApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.team_5.studygroup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StudygroupApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
