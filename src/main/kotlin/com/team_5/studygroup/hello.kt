bafde85ef7b5fa3e8d85d543357f593daba4c787
donghyun <joshua2031@snu.ac.kr>
2026-01-07 23:37:37 +0900

    간단한 hello와 test파일 추가

---
+++ src/main/kotlin/com/team_5/studygroup/hello.kt
@@ -0,0 +1,13 @@
+package com.team_5.studygroup
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+
+    @GetMapping("/")
+    fun hello(): String {
+        return "Hello World"
+    }
+}
---
+++ src/test/kotlin/com/team_5/studygroup/HelloTest.kt
@@ -0,0 +1,15 @@
+package com.team_5.studygroup
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class HelloTest {
+
+    @Test
+    fun helloWorldTest() {
+        val expected = "Hello World"
+        val actual = "Hello World"
+
+        assertEquals(expected, actual)
+    }
+}
