037fdba66670804af19bdaa7c8260dcf78b948f8
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-01-12 22:25:54 +0900

    categories, sub_categories 추가 및 순서 수정

--- src/main/resources/db.migration/V1__create_table_users.sql
+++ src/main/resources/db.migration/V1__create_table_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS users
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
     major VARCHAR(255),
     student_number VARCHAR(255),
     nickname VARCHAR(255),
--- src/main/resources/db.migration/V4__create_table_groups.sql
+++ src/main/resources/db.migration/V4__create_table_groups.sql
@@ -3,12 +3,14 @@ CREATE TABLE IF NOT EXISTS groups
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     group_name VARCHAR(255) NOT NULL,
     description TEXT,
-    category_id INT,
-    sub_category_id INT,
+    category_id BIGINT,
+    sub_category_id BIGINT,
     capacity INT,
     leader_id BIGINT,
     is_online BOOLEAN,
     status VARCHAR(255),
     created_at TIMESTAMP,
-    updated_at TIMESTAMP
+    updated_at TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES categories(id),
+    FOREIGN KEY (sub_category_id) REFERENCES sub_categories(id)
 );
