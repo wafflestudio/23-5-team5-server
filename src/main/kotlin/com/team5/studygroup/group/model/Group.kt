b6ab57d80b1b41ee263fe7217206403654e8bbbe
ezypuz <ezypuz@snu.ac.kr>
2026-01-14 23:03:24 +0900

    [FEATURE] - AuthController.kt 변경 - Group.kt 변경 - UserGroup.kt 변경

--- src/main/kotlin/com/team5/studygroup/group/model/Group.kt
+++ src/main/kotlin/com/team5/studygroup/group/model/Group.kt
@@ -1,14 +1,24 @@
 package com.team5.studygroup.group.model
 
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.Table
 import org.springframework.data.annotation.CreatedDate
-import org.springframework.data.annotation.Id
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 
+@Entity
 @Table(name = "groups")
+@EntityListeners(AuditingEntityListener::class) // 생성/수정일 자동 기록을 위해 필요
 class Group(
-    @Id val id: Long? = null,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
     val groupName: String,
     val description: String,
     val categoryId: Int,
@@ -18,6 +28,7 @@ class Group(
     val isOnline: Boolean,
     val status: String,
     @CreatedDate
+    @Column(updatable = false)
     val createdAt: Instant? = null,
     @LastModifiedDate
     val updatedAt: Instant? = null,
--- src/main/kotlin/com/team5/studygroup/usergroup/model/UserGroup.kt
+++ src/main/kotlin/com/team5/studygroup/usergroup/model/UserGroup.kt
@@ -1,13 +1,17 @@
 package com.team5.studygroup.usergroup.model
 
 import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
 import jakarta.persistence.Table
-import org.springframework.data.annotation.Id
 
 @Entity
 @Table(name = "user_groups")
 class UserGroup(
-    @Id val id: Long? = null,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
     val groupId: Long,
     val userId: Long,
     val status: String,
