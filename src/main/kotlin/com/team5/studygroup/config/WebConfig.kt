d26f1942c46786f255f3facb8c4a5574cdf86a3d
donghyun <joshua2031@snu.ac.kr>
2026-01-16 21:11:29 +0900

    swagger..

--- src/main/kotlin/com/team5/studygroup/config/WebConfig.kt
+++ src/main/kotlin/com/team5/studygroup/config/WebConfig.kt
@@ -1,14 +1,12 @@
 package com.team5.studygroup.config
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.method.HandlerTypePredicate
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun configurePathMatch(configurer: PathMatchConfigurer) {
-        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController::class.java))
+//        configurer.addPathPrefix("/api", HandlerTypePredicate.forAnnotation(RestController::class.java))
     }
 }
