311c3cfc5648983f80562dd0c396c6cfc024fa90
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-01-26 23:19:25 +0900

    Feat/59 profile image download (#73)
    * 프로필 이미지 다운로드 구현
    
    * ProfileService에 별도의 함수 추가

--- src/main/kotlin/com/team5/studygroup/user/controller/UserController.kt
+++ src/main/kotlin/com/team5/studygroup/user/controller/UserController.kt
@@ -5,6 +5,7 @@ import com.team5.studygroup.user.dto.GetProfileDto
 import com.team5.studygroup.user.dto.UpdateProfileDto
 import com.team5.studygroup.user.service.ProfileService
 import io.swagger.v3.oas.annotations.Parameter
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
+import java.net.URI
 
 @RestController
 @RequestMapping("/users/me")
@@ -47,5 +49,20 @@ class UserController(
     ): ResponseEntity<GetProfileDto> {
         val response = profileService.updateProfileImage(userId, profileImage)
         return ResponseEntity.ok(response)
+    }
+
+    // 프로필 이미지 다운로드
+    @GetMapping("/profile-image")
+    fun getProfileImage(
+        @Parameter(hidden = true) @LoggedInUser userId: Long,
+    ): ResponseEntity<Void> {
+        val profileImageUrl = profileService.getProfileImageUrl(userId)
+        return if (profileImageUrl != null) {
+            ResponseEntity.status(HttpStatus.FOUND)
+                .location(URI.create(profileImageUrl))
+                .build()
+        } else {
+            ResponseEntity.notFound().build()
+        }
     }
 }
