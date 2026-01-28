2b13c6baef3d6560974ee12a434e07e5170c53cd
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-01-28 15:32:30 +0900

    빌드 수정 중

--- src/main/kotlin/com/team5/studygroup/review/model/Review.kt
+++ src/main/kotlin/com/team5/studygroup/review/model/Review.kt
@@ -3,12 +3,10 @@ package com.team5.studygroup.review.model
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
-import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
