ff90bf5f7c0a33c8ed0ea9bb31f24bc27ce56400
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-02-04 21:30:05 +0900

    UserSearchController에 반영

--- src/main/kotlin/com/team5/studygroup/user/controller/UserSearchController.kt
+++ src/main/kotlin/com/team5/studygroup/user/controller/UserSearchController.kt
@@ -2,6 +2,8 @@ package com.team5.studygroup.user.controller
 
 import com.team5.studygroup.user.LoggedInUser
 import com.team5.studygroup.user.dto.UserSearchResponseDto
+import com.team5.studygroup.user.service.UserSearchService
+import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
@@ -12,12 +14,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/users/search")
-class UserSearchController {
+class UserSearchController(
+    private val userSearchService: UserSearchService,
+) {
     @GetMapping("")
     fun searchUsersInGroup(
         @RequestParam groupId: Long,
         @LoggedInUser requestingUserId: Long,
         @PageableDefault(size = 10, sort = ["createdAt"]) pageable: Pageable,
-    ): ResponseEntity<UserSearchResponseDto> {
+    ): ResponseEntity<Page<UserSearchResponseDto>> {
+        val result = userSearchService.searchUsersInGroup(groupId, requestingUserId, pageable)
+        return ResponseEntity.ok(result)
     }
 }
