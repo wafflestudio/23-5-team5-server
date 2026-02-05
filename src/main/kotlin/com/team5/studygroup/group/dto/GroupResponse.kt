6882dd2cbe32ebf89a68b2a9ff88c7f2a46e879a
JunwooPark <76034729+kodugi@users.noreply.github.com>
2026-02-05 19:43:41 +0900

    [REFACTOR] ktlintFormat 적용

--- src/main/kotlin/com/team5/studygroup/group/dto/GroupResponse.kt
+++ src/main/kotlin/com/team5/studygroup/group/dto/GroupResponse.kt
@@ -2,10 +2,9 @@ package com.team5.studygroup.group.dto
 
 import com.team5.studygroup.group.GroupStatus
 import com.team5.studygroup.group.model.Group
-import com.team5.studygroup.user.model.User
 import java.time.Instant
 
-class GroupResponse (
+class GroupResponse(
     val id: Long,
     val groupName: String,
     val description: String,
@@ -19,20 +18,19 @@ class GroupResponse (
     val createdAt: Instant?,
 ) {
     companion object {
-        fun from(
-            group: Group,
-        ) = GroupResponse(
-            id = group.id!!,
-            groupName = group.groupName,
-            description = group.description,
-            categoryId = group.categoryId,
-            subCategoryId = group.subCategoryId,
-            capacity = group.capacity,
-            leaderId = group.leaderId,
-            isOnline = group.isOnline,
-            location = group.location,
-            status = group.status,
-            createdAt = group.createdAt,
-        )
+        fun from(group: Group) =
+            GroupResponse(
+                id = group.id!!,
+                groupName = group.groupName,
+                description = group.description,
+                categoryId = group.categoryId,
+                subCategoryId = group.subCategoryId,
+                capacity = group.capacity,
+                leaderId = group.leaderId,
+                isOnline = group.isOnline,
+                location = group.location,
+                status = group.status,
+                createdAt = group.createdAt,
+            )
     }
 }
