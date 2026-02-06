c0edf166f00f13a9cc4e6de2204c16d5386f8cdf
donghyun <joshua2031@snu.ac.kr>
2026-02-07 08:05:06 +0900

    Feat/get others profile (#98)
    * [FEAT] 다른 사람의 프로필 조회 기능 추가
    
    * ktlintFormat

---
+++ src/main/kotlin/com/team5/studygroup/user/dto/UserTargetDto.kt
@@ -0,0 +1,10 @@
+package com.team5.studygroup.user.dto
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class UserTargetDto(
+    @Schema(description = "조회할 대상 유저의 ID", example = "12")
+    @field:JsonProperty("user_id")
+    val userId: Long,
+)
